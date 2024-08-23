1a350b9641baed5fb295c2f504f31b08aa5af7f3
Nguyễn Duy Hưng <duyhung2h@gmail.com>
2024-08-23 17:02:51 +0700

    update pdf
    update pdf

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,11 +1,36 @@
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
 
+const [numPages, setNumPages] = useState(null);
+const [pageNumber, setPageNumber] = useState(1);
 
-export default function Sooundboard() {
+function onDocumentLoadSuccess({ numPages }: any) {
+  setNumPages(numPages);
+}
+
+export default function Resume() {
   return (
     <>
-    {/* <iframe src="https://www.topcv.vn/xem-cv/BlFWVV8HA1pQCwEBDwVQAwFVVFtWUlIGVAVXVA579f" width="100%" height="100%"></iframe> */}
-    
-    {/* <img src="/assets/test.jpg"></img> */}
+
+<div>
+      <Document
+        file="/assets/test.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>Page {pageNumber} of {numPages}</p>
+    </div>
+            <embed
+              src={"/assets/test.pdf"}
+              style={{
+                width: '100%',
+                height: '100%',
+                flexGrow: '0',
+                flexShrink: '1',
+              }}
+              type='application/pdf'
+            />
         <object data="/assets/test.pdf" type="application/pdf" width="100%" height="100%">
       <p>Alternative text - include a link <a href="/assets/resume.pdf">to the PDF!</a></p>
   </object>
