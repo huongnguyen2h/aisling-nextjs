eda997c7b2662082be5d3eb1d16d70662bd71c87
Nguyễn Duy Hưng <40296674+duyhung2h@users.noreply.github.com>
2024-08-24 05:51:17 +0700

    test

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,39 +1,41 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf';
 
-const [numPages, setNumPages] = useState(null);
-const [pageNumber, setPageNumber] = useState(1);
+function Resume() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-function onDocumentLoadSuccess({ numPages }: any) {
-  setNumPages(numPages);
-}
 
-export default function Resume() {
+  function onDocumentLoadSuccess({ numPages }: any) {
+    setNumPages(numPages);
+  }
+  
   return (
-    <>
-
-<div>
-      <Document
-        file="/assets/test.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>Page {pageNumber} of {numPages}</p>
-    </div>
-            <embed
-              src={"/assets/test.pdf"}
-              style={{
-                width: '100%',
-                height: '100%',
-                flexGrow: '0',
-                flexShrink: '1',
-              }}
-              type='application/pdf'
-            />
-        <object data="/assets/test.pdf" type="application/pdf" width="100%" height="100%">
-      <p>Alternative text - include a link <a href="/assets/resume.pdf">to the PDF!</a></p>
-  </object>
-    </>
+    <div>
+      <div>
+        {/* <Document
+          file="/assets/test.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+        > */}
+          {/* <Page pageNumber={pageNumber} /> */}
+        {/* </Document> */}
+        <p>Page {pageNumber} of {numPages}</p>
+      </div>
+      <embed
+        src={"/assets/test.pdf"}
+        style={{
+          width: '100%',
+          height: '100%',
+          flexGrow: '0',
+          flexShrink: '1',
+        }}
+        type='application/pdf'
+      />
+      <object data="/assets/test.pdf" type="application/pdf" width="100%" height="100%">
+        <p>Alternative text - include a link <a href="/assets/resume.pdf">to the PDF!</a></p>
+      </object>
+  </div>
   )
 }
+
+export default Resume;
