ff7794eacd7a52800222343d16048cd2fea1e863
Nguyễn Duy Hưng <duyhung2h@gmail.com>
2024-08-27 15:58:01 +0700

    Update CommonService.tsx

--- db/CommonService.tsx
+++ db/CommonService.tsx
@@ -1,12 +1,12 @@
 
 
-function authenticateUser(user: any, password: any)
-{
-    var token = user + ":" + password;
+// function authenticateUser(user: any, password: any)
+// {
+//     var token = user + ":" + password;
 
-    // Should i be encoding this value????? does it matter???
-    // Base64 Encoding -> btoa
-    var hash = btoa(token); 
+//     // Should i be encoding this value????? does it matter???
+//     // Base64 Encoding -> btoa
+//     var hash = btoa(token); 
 
-    return "Basic " + hash;
-}
+//     return "Basic " + hash;
+// }
