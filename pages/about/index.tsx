d1b48ee2698feb36abbaa0b0425c0486b4031901
Nguyễn Duy Hưng <40296674+duyhung2h@users.noreply.github.com>
2024-12-21 00:37:10 +0700

    Update index.tsx

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -11,6 +11,7 @@ import Head from 'next/head'
 function About() {
   const [page, setPage] = useState(1);
 	const containerRef = useRef(null);
+  const [result, setResult] = useState<any | null>(null);
 
   const getResume = async () => {
     const params = new URLSearchParams(window.location.search);
@@ -39,8 +40,114 @@ function About() {
     // return <div ref={containerRef} style={{ height: '100vh' }} />
   }
 
+  async function bubbleSort(arr) {
+    let n = arr.length;
+    // alert(1)
+
+    for (let i = 0; i < n-1; i++) {
+      for (let j = 0; j < n-1-i; j++) {
+        if (arr[j] > arr[j+1]) {
+          [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+        }
+      }
+    }
+    return arr
+  }
+
+  async function findArrayWithLargestSum(arrays) {
+    let largestSumArray = arrays[0];
+    let largestSum = largestSumArray.reduce((sum, num) => sum + num, 0);
+
+    arrays.forEach(array => {
+      const currentSum = array.reduce((sum, num) => sum + num, 0);
+      if (currentSum > largestSum) {
+        largestSum = currentSum;
+        largestSumArray = array
+      }
+    }) 
+    return largestSumArray
+  }
+  
+  async function findArrayWithElements(arrays, element1, element2) {
+    return arrays.filter(array => array.includes(element1) && array.includes(element2))
+  }
+
+  async function createMatrix(rows, cols, min, max) {
+    const matrix : any[] = [];
+    for (let i = 0; i < rows; i++) {
+      const row : any[] = [];
+      for (let j = 0; j < cols; j++) {
+        const randomNumber = Math.floor(Math.random() * (max-min +1)) + min
+        row.push(randomNumber);
+      }
+      matrix.push(row)
+    }
+    return matrix;
+  }
+
+  function isPrime(num) {
+    if(num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+
+  async function findPrimesInMatrix(matrix) {
+    let primes : any[] = [];
+
+    matrix.forEach((row, rowIndex) => {
+      row.forEach((value, colIndex) => {
+        if (isPrime(value)) {
+          primes.push({value, position: [rowIndex, colIndex]})
+        }
+      });
+    })
+    return primes
+  }
+
 	useEffect(() => {
-    getResume()
+    // getResume()
+    const foo = async () =>{
+      let arr = []
+      arr = await bubbleSort([60, 40, 69, 65, 55, 86, 81, 3, 99, 83, 6, 70, 80, 2, 66, 98])
+      // alert(2)
+      setResult(arr)
+      console.log(arr);
+      
+    };
+    foo()
+    const foo2 = async () =>{
+      let arr = []
+      arr = await findArrayWithLargestSum([[60, 40, 69, 65, 55],
+        [86,81,3,99,83],
+        [70,80,2,66,98]])
+      // alert(2)
+      setResult(arr)
+      console.log(arr);
+      
+    };
+    foo2()
+    const foo3 = async () =>{
+      let arr = []
+      arr = await findArrayWithElements([[60, 40, 69, 65, 55],
+        [86,81,1,99,83],
+        [70,80,2,66,98]], 1, 99)
+      // alert(2)
+      setResult(arr)
+      console.log(arr);
+      
+    };
+    foo3()
+    const foo4 = async () =>{
+      let matrix : any[] = [];
+      matrix = await createMatrix(100, 100, 1, 100000)
+      console.log(matrix);
+      let primes = await findPrimesInMatrix(matrix);
+      console.log(primes);
+      
+    };
+    foo4()
 	}, []);
 
   return (
@@ -49,6 +156,7 @@ function About() {
         <title>About page</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      {result}
       {/* <div ref={containerRef} style={{ height: '100vh' }} /> */}
     </div>
   )
