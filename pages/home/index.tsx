dd6679e5c2e32559bba24025f5055940205cb312
Nguyễn H. <40296674+huongnguyen2h@users.noreply.github.com>
2025-04-14 13:47:50 +0700

    update navbar
    update navbar

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,49 +1,7 @@
-import { FC, useEffect } from 'react'
-import type { AppProps } from 'next/app'
-import Layout from '@components/common/Layout'
 import Head from 'next/head'
-// import _const from '@components/common/const'
-// import { builder, Builder } from '@builder.io/react'
-// import builderConfig from '@config/builder'
-// builder.init(builderConfig.apiKey)
 
-// import '@builder.io/widgets'
-// import '../blocks/ProductGrid/ProductGrid.builder'
-// import '../blocks/CollectionView/CollectionView.builder'
-// import '../blocks/ProductView/ProductView.builder'
-// import '../blocks/CloudinaryImage/CloudinaryImage.builder'
+export default function Home() {
 
-// Builder.register('insertMenu', {
-//   name: 'Shopify Collections Components',
-//   items: [
-//     { name: 'CollectionBox', label: 'Collection stuff' },
-//     { name: 'ProductCollectionGrid' },
-//     { name: 'CollectionView' },
-//   ],
-// })
-
-// Builder.register('insertMenu', {
-//   name: 'Shopify Products Components',
-//   items: [
-//     { name: 'ProductGrid' },
-//     { name: 'ProductBox' },
-//     { name: 'ProductView' },
-//   ],
-// })
-
-// Builder.register('insertMenu', {
-//   name: 'Cloudinary Components',
-//   items: [{ name: 'CloudinaryImage' }],
-// })
-
-const Noop: FC<{ children: React.ReactNode }> = ({ children }) => (
-  <>{children}</>
-)
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  // pageProps = ''
-
-  console.log('_app pageProps: ', pageProps)
   let WEB_TITLE = "Aisling's Planet"
   let HOME = `Home`
   
@@ -52,7 +10,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const twitchParentDomain = 'localhost' 
 
   return (
-    <Layout pageProps={pageProps}>
+    <div>
       <Head>
         <title>
           {WEB_TITLE} - {HOME}
@@ -83,6 +41,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
       {/* You can add other homepage content below */}
       {/* <Component {...pageProps} /> */}
-    </Layout>
+    </div>
   )
 }
